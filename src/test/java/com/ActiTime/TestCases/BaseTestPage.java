9f8ea056eb6866586e05f64702a617e047ee0357
sandesh-pai <74829731+sandesh-pai@users.noreply.github.com>
2021-05-30 21:33:03 +0530

    updated-commit

--- src/test/java/com/ActiTime/TestCases/BaseTestPage.java
+++ src/test/java/com/ActiTime/TestCases/BaseTestPage.java
@@ -1,13 +1,9 @@
 package com.ActiTime.TestCases;
 
-import org.junit.BeforeClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
 
 import com.ActiTime.Driver.Driver;
-import com.ActiTime.ExtentReporter.ExtentManager;
 
 public class BaseTestPage {
 	
